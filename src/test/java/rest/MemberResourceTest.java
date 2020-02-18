a3f618526b60ac4f043cfa8f101c4ffba3ad7422
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2020-02-18 09:58:40 +0100

    Updated POM

---
+++ src/test/java/rest/MemberResourceTest.java
@@ -0,0 +1,88 @@
+package rest;
+/*
+ * author benjaminp
+ * version 1.0
+ */
+
+import entity.Member;
+import rest.ApplicationConfig;
+import utils.EMF_Creator;
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.parsing.Parser;
+
+import java.net.URI;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+
+public class MemberResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    private static Member r1, r2;
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
+
+        httpServer = startServer();
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        r1 = new Member(); //("Some txt","More text");
+        r2 = new Member(); //("aaa","bbb");
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Member.deleteAllRows").executeUpdate();
+            em.persist(r1);
+            em.persist(r2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testServerIsUp() {
+        given().when().get("/member").then().statusCode(200);
+    }
+}
